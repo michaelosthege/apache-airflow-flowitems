4e8c467be4006553b5674abeeb1dcca92157eac1
Michael Osthege <michael.osthege@outlook.com>
2022-07-16 14:16:45 +0200

    Refactor for compatibility with `XComArg` inputs

--- apache_airflow_flowitems/core.py
+++ apache_airflow_flowitems/core.py
@@ -1,40 +1,52 @@
 import abc
 import copy
 import inspect
-from typing import Any, Callable, Mapping, Sequence
+from typing import Any, Callable, Dict, List, Mapping, Sequence, Union
 
-from airflow.models import BaseOperator
+from airflow.models import BaseOperator, Operator
+from airflow.models.xcom_arg import XComArg
 from airflow.operators.python import PythonOperator
 
 
 class FlowItem:
-    def __init__(self, *args: Sequence[BaseOperator], **kwargs: Mapping[str, Any]) -> None:
+    def __init__(self, *args: Sequence[Union[Operator, XComArg]], **kwargs: Mapping[str, Any]) -> None:
         """Creates a FlowItem.
 
         Parameters
         ----------
         *args
-            Upstream tasks (BaseOperator instances) to depend on.
+            Upstream tasks (Operator instances) to depend on.
         *kwargs
             Named parameters of this tasks.
             The values can be constants,
-            or upstream tasks (BaseOperator instances) from which
+            or upstream tasks (Operator instances) from which
             to xcom_pull the return values.
         """
-        self.upstream_tasks = list(args)
-        self.kwargs_upstream = {}
-        self.kwargs_static = {}
+        self.upstream_tasks: List[Operator] = []
+        for iv, v in enumerate(args):
+            if isinstance(v, BaseOperator):
+                self.upstream_tasks.append(v)
+            elif isinstance(v, XComArg):
+                self.upstream_tasks.append(v.operator)
+            else:
+                raise NotImplementedError(f"Unsupported upstream dependency type {type(v)} in position {iv}.")
+
+        self.kwargs_xcom: Dict[str, XComArg] = {}
+        self.kwargs_static: Dict[str, Any] = {}
 
         for k, v in kwargs.items():
             if isinstance(v, BaseOperator):
-                self.kwargs_upstream[k] = v.task_id
+                self.kwargs_xcom[k] = v.output
                 self.upstream_tasks.append(v)
+            elif isinstance(v, XComArg):
+                self.kwargs_xcom[k] = v
+                self.upstream_tasks.append(v.operator)
             else:
                 self.kwargs_static[k] = v
         super().__init__()
 
     @abc.abstractmethod
-    def __call__(self, **kwargs: Mapping[str, Any]) -> BaseOperator:
+    def __call__(self, **kwargs: Mapping[str, Any]) -> Operator:
         """Creates an operator instance for a `FlowItem`.
 
         Parameters
@@ -45,7 +57,7 @@ class FlowItem:
 
         Returns
         -------
-        task : PythonOperator
+        task : Operator
             Instance of the created operator.
         """
         raise NotImplementedError()
@@ -53,7 +65,10 @@ class FlowItem:
 
 class PythonItem(FlowItem):
     def __init__(
-        self, python_callable: Callable, *args: Sequence[BaseOperator], **kwargs: Mapping[str, Any]
+        self,
+        python_callable: Callable,
+        *args: Sequence[Union[Operator, XComArg]],
+        **kwargs: Mapping[str, Any],
     ) -> None:
         self.python_callable = python_callable
         super().__init__(*args, **kwargs)
@@ -71,13 +86,11 @@ class PythonItem(FlowItem):
         result_json : str
             JSON-encoded return value of the callable.
         """
-        task_instance = context["task_instance"]
-
         # assemble the kwargs in a new dict
-        kwargs = copy(self.kwargs_static)
+        kwargs = copy.copy(self.kwargs_static)
 
-        for k, taskid in self.kwargs_upstream.items():
-            kwargs[k] = task_instance.xcom_pull(task_ids=[taskid])[0]
+        for k, xarg in self.kwargs_xcom.items():
+            kwargs[k] = xarg.resolve(context)
 
         # Forward the context dict if the callable takes a "context" parameter
         spec = inspect.getfullargspec(self.python_callable)
@@ -88,12 +101,12 @@ class PythonItem(FlowItem):
         return result
 
     def __call__(self, **kwargs: Mapping[str, Any]) -> PythonOperator:
-        """Creates a PythonOperator for the execution of this flow item.
+        """Creates a ``PythonOperator`` for the execution of this flow item.
 
         Parameters
         ----------
         **kwargs
-            Additional kwargs for the PythonOperator.
+            Additional kwargs for the ``PythonOperator``.
             For example: ``task_id``.
 
         Returns
--- apache_airflow_flowitems/test_core.py
+++ apache_airflow_flowitems/test_core.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from unittest.mock import MagicMock
 
 import pytest
 from airflow.decorators import dag
@@ -16,13 +17,32 @@ class TestFlowItem:
         assert up1 in fi.upstream_tasks
         assert up2 in fi.upstream_tasks
         assert fi.kwargs_static.get("kw1") == "one"
-        assert fi.kwargs_upstream.get("kw2") == "bash_2"
+        assert fi.kwargs_xcom.get("kw2") == up2.output
         pass
 
     def test_exceptions(self):
         fi = core.FlowItem()
         with pytest.raises(NotImplementedError):
             fi()
+
+        with pytest.raises(NotImplementedError, match="dependency type"):
+            core.FlowItem("not a task")
+        pass
+
+    def test_xcom_args(self):
+        up1 = BashOperator(bash_command="echo 1", task_id="bash_1").output
+        fi = core.FlowItem(up1, up1)
+        assert not fi.kwargs_static
+        assert not fi.kwargs_xcom
+        assert up1.operator in fi.upstream_tasks
+        pass
+
+    def test_xcom_kwargs(self):
+        up1 = BashOperator(bash_command="echo 1", task_id="bash_1").output
+        fi = core.FlowItem(up1, kw1=up1)
+        assert not fi.kwargs_static
+        assert fi.kwargs_xcom["kw1"] == up1
+        assert up1.operator in fi.upstream_tasks
         pass
 
 
@@ -43,7 +63,7 @@ class TestPythonItem:
         assert up1 in pi.upstream_tasks
         assert up2 in pi.upstream_tasks
         assert not pi.kwargs_static
-        assert pi.kwargs_upstream["kw1"] == "bash_2"
+        assert pi.kwargs_xcom["kw1"] == up2.output
         pass
 
     @dag("testdag", start_date=datetime.utcnow())
@@ -60,4 +80,29 @@ class TestPythonItem:
 
         pi_task_named = pi(task_id="into_dict")
         assert pi_task_named.task_id == "into_dict"
+        pass
+
+    def test_run(self):
+        x1 = BashOperator(bash_command="echo 1", task_id="bash_1").output
+        pi = core.PythonItem(TestPythonItem.fun_dict, a=x1)
+        assert not pi.kwargs_static
+        assert pi.kwargs_xcom["a"] == x1
+
+        x1.resolve = MagicMock(return_value="hello")
+        context = {"task_instance": None}
+        result = pi._run(**context)
+        x1.resolve.assert_called_once_with(context)
+        assert isinstance(result, dict)
+        assert result["a"] == "hello"
+        pass
+
+    def test_run_autocontext(self):
+        def fun_with_context(context):
+            return context["run_id"]
+
+        pi = core.PythonItem(fun_with_context)
+
+        context = {"run_id": "manual__123"}
+        result = pi._run(**context)
+        assert result == "manual__123"
         pass
